788f2b5e3b3649d4c5c7ae73c239f0e274db8c8c
Azizul Dzikri <90754841+azizuldz@users.noreply.github.com>
2023-10-09 21:46:07 +0700

    change file py

---
+++ polyalphabet.py
@@ -0,0 +1,18 @@
+def polyalphabet_cipher(plaintext, key):
+    plaintext = plaintext.upper().replace(" ", "").replace(".", "").replace(",", "")
+    alphabets = []
+    for i in range(len(key)):
+        shift = ord(key[i]) - 65
+        alphabets.append(alphabet)
+    ciphertext = ""
+    for i in range(len(plaintext)):
+        alphabet_index = i % len(key)
+        alphabet = alphabets[alphabet_index]
+        letter_index = ord(plaintext[i]) - 65
+        ciphertext += alphabet[letter_index]
+    return ciphertext
+
+plaintext = input("Masukkan teks yang akan dienkripsi: ")
+key = "selasa"
+ciphertext = polyalphabet_cipher(plaintext, key)
+print("Hasil enkripsi:", ciphertext)
